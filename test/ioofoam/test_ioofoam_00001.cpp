eb6ffb816d102cbea1272e1716e32495b7812438
roccoarpa <rocco.arpa@optimad.it>
2017-04-03 18:53:39 +0200

    mimmo: updating cmake tree for tests
    Actually there are no tests available. Need to be provided for each module of the library
    strictly working on the module containts.
    
    Old tests are collected in the folder oldtests. Need to be reupped and remanaged as examples.
    Will be perfect if an example reports with a c++ code and xml-tui dictionary

---
+++ test/ioofoam/test_ioofoam_00001.cpp
@@ -0,0 +1,63 @@
+/*---------------------------------------------------------------------------*\
+ * 
+ *  MiMMO
+ *
+ *  Copyright (C) 2015-2016 OPTIMAD engineering Srl
+ *
+ *  -------------------------------------------------------------------------
+ *  License
+ *  This file is part of mimmo.
+ *
+ *  MiMMO is free software: you can redistribute it and/or modify it
+ *  under the terms of the GNU Lesser General Public License v3 (LGPL)
+ *  as published by the Free Software Foundation.
+ *
+ *  MiMMO is distributed in the hope that it will be useful, but WITHOUT
+ *  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ *  FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+ *  License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with MiMMO. If not, see <http://www.gnu.org/licenses/>.
+ *
+ \ *---------------------------------------------------------------------------*/
+
+#include "mimmo.hpp"
+using namespace std;
+using namespace bitpit;
+using namespace mimmo;
+
+
+
+// =================================================================================== //
+
+int test1() {
+	
+	std::cout<<"waiting for a proper test. I do nothing for now"<<std::endl;
+    return 1;
+}
+
+// =================================================================================== //
+
+int main( int argc, char *argv[] ) {
+
+	BITPIT_UNUSED(argc);
+	BITPIT_UNUSED(argv);
+	
+#if ENABLE_MPI==1
+	MPI::Init(argc, argv);
+
+	{
+#endif
+		/**<Calling MiMMO Test routines*/
+
+        int val = test0001() ;
+
+#if ENABLE_MPI==1
+	}
+
+	MPI::Finalize();
+#endif
+	
+	return val;
+}
