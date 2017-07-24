9eb5084a69557b8d064de683fecf7ca7b8ffeba8
roccoarpa <rocco.arpa@optimad.it>
2017-07-24 17:34:19 +0200

    utils: fixed the other mvp issues

--- src/utils/ControlDeformExtSurface.cpp
+++ src/utils/ControlDeformExtSurface.cpp
@@ -113,7 +113,7 @@ double
 ControlDeformExtSurface::getViolation(){
 
     double result = -1.0E+18;
-    for(const auto & val : m_violationField){
+    for(const auto & val : m_violationField.data()){
         result = std::fmax(result, val);
     }
 
@@ -288,7 +288,7 @@ ControlDeformExtSurface::execute(){
     long int ID;
     for (const auto & v : geo->getVertices()){
         ID = v.getId();
-        m_violationField.insert(ID, -1.0e+18);
+        m_violationField.data().insert(ID, -1.0e+18);
     }
 
     dvector1D violationField;
@@ -513,7 +513,7 @@ ControlDeformExtSurface::execute(){
         }//end if else
 
         int ii = 0;
-        for(auto & val : m_violationField){
+        for(auto & val : m_violationField.data()){
             val = std::fmax(val, (violationField[ii] + tols[counterExtGeo]));
             ++ii;
         }
@@ -739,7 +739,7 @@ ControlDeformExtSurface::plotOptionalResults(){
     dvecarr3E  points = getGeometry()->getVertexCoords(&map);
     dvecarr3E deff(m_defField.size());
     int count = 0;
-    for (const auto & f : m_defField){
+    for (const auto & f : m_defField.data()){
         deff[count] = f;
         ++count;
     }
@@ -757,7 +757,7 @@ ControlDeformExtSurface::plotOptionalResults(){
 
     dvector1D viol(m_violationField.size());
     count = 0;
-    for (const auto & f : m_violationField){
+    for (const auto & f : m_violationField.data()){
         viol[count] = f;
         ++count;
     }
