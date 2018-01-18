c1ddcfe18bac8694a60243ce9beeb32a4df0f55d
roccoarpa <rocco.arpa@optimad.it>
2018-01-18 19:12:48 +0100

    test: added new slot for modules system and propagators

---
+++ test/propagators/test_propagators_00001.cpp
@@ -0,0 +1,64 @@
+/*---------------------------------------------------------------------------*\
+ * 
+ *  mimmo
+ *
+ *  Copyright (C) 2015-2017 OPTIMAD engineering Srl
+ *
+ *  -------------------------------------------------------------------------
+ *  License
+ *  This file is part of mimmo.
+ *
+ *  mimmo is free software: you can redistribute it and/or modify it
+ *  under the terms of the GNU Lesser General Public License v3 (LGPL)
+ *  as published by the Free Software Foundation.
+ *
+ *  mimmo is distributed in the hope that it will be useful, but WITHOUT
+ *  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ *  FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+ *  License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with mimmo. If not, see <http://www.gnu.org/licenses/>.
+ *
+ \ *---------------------------------------------------------------------------*/
+
+#include "mimmo_propagators.hpp"
+#include <exception>
+using namespace std;
+using namespace bitpit;
+using namespace mimmo;
+
+
+
+// =================================================================================== //
+
+int test1() {
+	
+	std::cout<<"Waiting for a proper test. I do nothing for now"<<std::endl;
+    return 0;
+}
+
+// =================================================================================== //
+
+int main( int argc, char *argv[] ) {
+
+	BITPIT_UNUSED(argc);
+	BITPIT_UNUSED(argv);
+	
+#if ENABLE_MPI==1
+	MPI::Init(argc, argv);
+
+	{
+#endif
+		/**<Calling mimmo Test routines*/
+
+        int val = test1() ;
+
+#if ENABLE_MPI==1
+	}
+
+	MPI::Finalize();
+#endif
+	
+	return val;
+}
